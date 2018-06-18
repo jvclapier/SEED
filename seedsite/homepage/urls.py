8b6c95abcc566df79e82b217515b24de174afa70
Jess Clapier <jvclapier@gmail.com>
2018-06-18 16:06:07 +0800

    added bootstrap and base files

--- seedsite/homepage/urls.py
+++ seedsite/homepage/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from homepage import views
 
 urlpatterns = [
-    path('', include('homepage.urls')),
-    path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
--- seedsite/homepage/views.py
+++ seedsite/homepage/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from django.core.mail import send_mail
+import re
+from django.template.loader import render_to_string, get_template
+from django.conf import settings as django_settings
 
 # Create your views here.
+
+def index(request):
+
+    context = {
+
+    }
+
+    return render(request, 'homepage/index.html', context)
