14c7d9b1ebb0bf4930dbb9f6a89393f10c9a6214
Jess Clapier <jvclapier@gmail.com>
2018-06-20 19:37:52 +0800

    finished DB

--- seedsite/homepage/models.py
+++ seedsite/homepage/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime, date
+from django.conf import settings
 
-# Create your models here.
+class Intern(AbstractUser):
+    # username
+    # password
+    first_name = models.TextField(blank=True, null=True)
+    last_name = models.TextField(blank=True, null=True)
+    email = models.EmailField()
+    semester = models.TextField(blank=True, null=True)
+
+class AssignedClient(models.Model):
+    intern_id = models.ForeignKey('Intern', on_delete=models.CASCADE)
+    client_id = models.ForeignKey('Client', on_delete=models.CASCADE)
+
+class Client(models.Model):
+    first_name = models.TextField(blank=True, null=True)
+    last_name = models.TextField(blank=True, null=True)
+    gender = models.TextField(blank=True, null=True)
+    email = models.EmailField()
+    phone_number = models.CharField(blank=True, null=True, max_length=11)
+    tagalog_needed = models.BooleanField(default=True)
+    street_address = models.TextField(blank=True, null=True)
+    city = models.TextField(blank=True, null=True)
+    zipcode = models.TextField(blank=True, null=True)
+    country = models.TextField(blank=True, null=True)
+    barangay = models.TextField(blank=True, null=True)
+    # TODO: need to add lat and long coordinates, look at google API
+    business_name = models.TextField(blank=True, null=True)
+    business_type = models.TextField(blank=True, null=True)
+    transportation_method = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+class Log(models.Model):
+    date_created = models.DateField(auto_now_add=True)
+    visit_description = models.TextField(blank=True, null=True)
+    next_steps = models.TextField(blank=True, null=True)
+    time_of_visit = models.TextField(blank=True, null=True)
+    intern_id = models.ForeignKey('Intern', on_delete=models.CASCADE)
+    client_id = models.ForeignKey('Client', on_delete=models.CASCADE)
