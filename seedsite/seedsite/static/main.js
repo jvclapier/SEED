6208f3767d566009a16a1da44657a911414780d6
Jess Clapier <jvclapier@gmail.com>
2018-07-18 15:11:40 +0800

    tab switching active

--- seedsite/seedsite/static/main.js
+++ seedsite/seedsite/static/main.js
@@ -1,5 +1,22 @@
+
 $('.card-body').click(function() {
     console.log('card flip function called');
     $(this).closest('.card-flip').toggleClass('hover');
     $(this).css('transform, rotateY(180deg)');
 });
+
+$( '#bottom-navbar .nav-link i' ).on( 'click', function () {
+	$( '#bottom-navbar .nav-link' ).find( 'i.active' ).removeClass( 'active' );
+	$( this ).addClass( 'active' );
+});
+
+var setDefaultActive = function() {
+    var currentPage = $(".where-am-i").text();
+    var selectedTab = $('.' + currentPage);
+
+    console.log(selectedTab.text());
+
+    selectedTab.addClass('active');
+}
+
+setDefaultActive()
