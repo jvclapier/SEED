a0701113041a96eee5fbac96dd06d68c51f1d30c
bentonsmith <39981944+bentonsmith@users.noreply.github.com>
2018-11-09 16:25:16 -0700

    Static files again

--- seedsite/settings.py
+++ seedsite/settings.py
@@ -134,8 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'seedsite', 'static'),
-    #os.path.join(BASE_DIR, "static"),
+    #os.path.join(BASE_DIR, 'seedsite', 'static'),
+    os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 )
 
